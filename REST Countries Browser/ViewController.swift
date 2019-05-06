539e477da91f73cd58205008e4137d9a8174c1ac
AC <czyzyk@icloud.com>
2019-05-06 11:24:32 +0200

    Initial Commit

---
+++ REST Countries Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  REST Countries Browser
+//
+//  Created by Andrzej Czyżyk on 06/05/2019.
+//  Copyright © 2019 Andrzej Czyżyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ REST Countries BrowserTests/REST_Countries_BrowserTests.swift
@@ -0,0 +1,34 @@
+//
+//  REST_Countries_BrowserTests.swift
+//  REST Countries BrowserTests
+//
+//  Created by Andrzej Czyżyk on 06/05/2019.
+//  Copyright © 2019 Andrzej Czyżyk. All rights reserved.
+//
+
+import XCTest
+@testable import REST_Countries_Browser
+
+class REST_Countries_BrowserTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
